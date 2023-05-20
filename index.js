d4823166100764bbc7a03bd391ef6476613b4bc5
Dinh Nguyen <nguyen_dinh282@yahoo.com>
2023-05-20 07:16:10 -0400

    refactor code so application contains Triangle, Square, and Circle classes

--- index.js
+++ index.js
@@ -2,15 +2,21 @@
 //to comply with ES 6 since our version of inquirer is later than 8.2.4
 import  inquirer  from 'inquirer';
 import  fs  from 'fs';
-import  CreateLogo from './lib/shapes.js';
+import  { Circle, Triangle, Square } from './lib/shapes.js';
+//color-name package provides a list of names of colors that are supported by SVG.
 import colorNames from 'color-name';
-// import colorNames from './node_modules/color-name-list/dist/colornames.json'
 
+//we use Object.keys() to extract te color names from the colorNames object.
 const validColors = Object.keys(colorNames);
 
+//this function takes in the array of validColors and the color value that inquirer obtains from 
+//the user to check of what the user entered exists in the array.
 function checkColor (validColors, color) {
-        const colorRegEx = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
 
+        const colorRegEx = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+        //.test() method is a regular expression method in JS. it tests whethera string matches 
+        // a regular expression pathern. in this case, it is checking whether what the user entered
+        //matches with the pattern of colorRegEx.
         if(colorRegEx.test(color) || validColors.includes(color.toLowerCase())) {
             return true;
         }else {
@@ -22,15 +28,16 @@ function checkColor (validColors, color) {
  inquirer
         .prompt([
           {
+            //this checks if the length of the string the user input is equal to or less than 3.
             validate: (str) => str.length <=3, 
             name: 'text',
             message: 'Enter text for the logo. (Must not b more than 3 characters.)',
           },
           {
+            //this checks the color name or Hex code entered by user by calling the checkColor() function. 
             validate: (color) => checkColor(validColors, color),
-            // type: 'input',
             name: 'textColor',
-            message: 'Enter the text color name or Hexadecimal color code.',
+            message: 'Enter a color name or Hexadecimal color code for the text color.',
           },
           {
             type: 'list',
@@ -40,19 +47,35 @@ function checkColor (validColors, color) {
           },
           {
             validate: (color) => checkColor(validColors, color),
-            // type:'input',
             name: 'shapeColor',
-            message: 'Enter a shape color.',
+            message: 'Enter a color name or Hexadecimal color code for the shape color.',
           },
         ])
         //answers is parameter to the => function that we obtain from inquirer.prompt().
         .then((answers) => {
-            //we destructure to extract the properties from answer object and assign them to the variables
-            //text, textColor, shape, shapeColor
+            //we extract the properties from answer objectand assign them to the variables
+            //text, textColor, shape, shapeColor using destructuring syntax.
             const {text, textColor, shape, shapeColor} = answers;
             //we instantiate CreateLogo(with the variables as arguments) and call the createLogo() method
-            //and whatever is returned from createLogo() we are saving to variabel content.
-            const content =  new CreateLogo(text, textColor, shape, shapeColor).createLogo() 
+            //and with whatever that is returned from createLogo(), we are saving to variabel content.
+            var logo;
+
+            switch (shape) {
+                case 'circle':
+                    logo = new Circle(text, textColor, shapeColor);
+                    break;
+                case 'triangle':
+                    logo = new Triangle(text, textColor, shapeColor);
+                    break;
+                case 'square':
+                    logo = new Square(text, textColor, shapeColor);
+                    break;
+                default:
+                    throw new Error('Not a valid shape!')
+            }
+
+            const content = logo.createLogo();
+
             // fs.promises.writeFile() method is used to generate the svg file. we pass in svg file name,
             // and the data to be written (content).
            return fs.promises.writeFile(
